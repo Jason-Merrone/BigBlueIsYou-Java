835f6b833959d99bcc741616525b9d8667390858
Jason Merrone <jasonmerrone@gmail.com>
2025-04-15 20:02:39 -0600

    First commit - basic menu and ECS framework

---
+++ src/GameModel.java
@@ -0,0 +1,19 @@
+import ecs.Entities.*;
+import edu.usu.graphics.*;
+
+public class GameModel {
+
+    public void initialize(Graphics2D graphics) {
+
+    }
+
+    public void update(double elapsedTime) {
+    }
+
+    private void addEntity(Entity entity) {
+    }
+
+    private void removeEntity(Entity entity) {
+    }
+
+}
